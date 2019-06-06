b64d9b2e8f327592805fe3116ab759909917499b
KeranYe <kyeaquarius@gmail.com>
2019-06-05 20:40:17 -0700

    files modified

--- scripts/my_controller_2d_circle.py
+++ scripts/my_controller_2d_circle.py
@@ -4,29 +4,26 @@ import math
 import numpy as np
 from crazyflieParser import CrazyflieParser
 
-def PointsInCircum(r,n=100):
+def PointsInCircum(r,n=100.0):
     return [[math.cos(2*pi/n*x)*r,math.sin(2*pi/n*x)*r] for x in range(0,n+1)]
 
 if __name__ == '__main__':
     pi = math.pi
     index = 1   # for cf1
-    initialPosition = [0,1.5,0] # x,y,z coordinate for this crazyflie
+    initialPosition = [0.0,1.5,0.0] # x,y,z coordinate for this crazyflie
     cfs = CrazyflieParser(index, initialPosition)
     cf = cfs.crazyflies[0]
     time = cfs.timeHelper
 
     # Get input for circle
-    num_wp = 100    
-    takeoff_height = float(raw_input("takeoff height:"))  
-    takeoff_duration = float(raw_input("takeoff duration:"))
-    center = map(float, raw_input("x,y of center= ").split())
-    #print np.shape(center)
-    radius = math.sqrt(np.linalg.norm(center))
+    num_wp = 100   
+    takeoff_height = 1.0  
+    takeoff_duration = 5.0
+    center = np.array([0.0,-0.5])
+    radius = 1.0
     circle = PointsInCircum(radius,n = num_wp)
     circle = np.reshape(circle, (-1,2))
     wp_list = circle + center
-    #print np.shape(wp_list)
-    #wp_list = np.reshape(wp_list, (-1,2))
     z_list = takeoff_height*(np.ones((num_wp+1,1)))
     wp_list = np.append(wp_list, z_list, axis = 1)
     
@@ -34,23 +31,23 @@ if __name__ == '__main__':
     cf.setParam("stabilizer/estimator", 2) # Use EKF
     cf.setParam("stabilizer/controller", 2) # Use mellinger controller
     #cf.setParam("ring/effect", 7)
-
+    
+    # Takeoff
     cf.takeoff(targetHeight = takeoff_height, duration = takeoff_duration)
     time.sleep(takeoff_duration)
+    
+    # Go to start point of the circle
+    cf.goTo(goal = wp_list[0,:], yaw = 0.0, duration = 5.0, relative = False, groupMask = 0)
+    time.sleep(5.0)
 
-    # FILL IN YOUR CODE HERE
-    # Please try both goTo and cmdPosition
-    #cf.goTo(goal = np.array([2,2,2]), yaw = 0.0, duration = 3.0, relative = False, groupMask = 0)
-    #time.sleep(3.0)
-    # Move along a segment
-    goto_duration = math.sqrt(np.linalg.norm(wp_list[0,:2]))/1.0
-    cf.goTo(goal = wp_list[0,:], yaw = 0.0, duration = goto_duration, relative = False, groupMask = 0)
-    time.sleep(goto_duration)
+    # GO circle
     for i in range(num_wp+1):
         cf.cmdPosition(wp_list[i,:], yaw = 0)
         time.sleep(0.1)
-
     
+    # GO back to landing
+    cf.goTo(goal = [0.0, 1.5, 1.0], yaw = 0.0, duration = 5.0, relative = False, groupMask = 0)
+    time.sleep(5.0)
 
     cf.land(targetHeight = 0.0, duration = 5.0)
     time.sleep(5.0)
--- scripts/my_controller_2d_eight.py
+++ scripts/my_controller_2d_eight.py
@@ -5,47 +5,54 @@ from math import sin, cos, pi
 import numpy as np
 from crazyflieParser import CrazyflieParser
 
-if __name__ == '__main__':
-    
-    index = 1   # for cf1
-    initialPosition = [0,1.5,0] # x,y,z coordinate for this crazyflie
-    cfs = CrazyflieParser(index, initialPosition)
-    cf = cfs.crazyflies[0]
-    time = cfs.timeHelper
-
+def eight_traj():
     t = np.linspace(0,2*pi,100)
     data = np.array([[0],[1],[1]])
     for i in range(100):
-        x = cos(t[i])
-        y = sin(t[i])
+        x = 0.5*cos(t[i])
+        y = 0.5*sin(t[i])
         current = np.array([[y],[x],[1]])
         data = np.append(data,current,axis =1)
     for i in range(100):
-        x = 2 - cos(t[i])
-        y = sin(t[i])
+        x = 1 - 0.5*cos(t[i])
+        y = 0.5*sin(t[i])
         current = np.array([[y],[x],[1]])
         data = np.append(data,current,axis =1)
     data = data.T
+    return data
+
+if __name__ == '__main__':
     
+    index = 1   # for cf1
+    initialPosition = [0.0, 1.5, 0.0] # x,y,z coordinate for this crazyflie
+    cfs = CrazyflieParser(index, initialPosition)
+    cf = cfs.crazyflies[0]
+    time = cfs.timeHelper
+    
+    # Get input for eight trajectory
+    eight_traj = eight_traj()
+   
     cf.setParam("commander/enHighLevel", 1)
     cf.setParam("stabilizer/estimator", 2) # Use EKF
     cf.setParam("stabilizer/controller", 2) # Use mellinger controller
-    #cf.setParam("ring/effect", 7)
-
-    cf.takeoff(targetHeight = 0.5, duration = 3.0)
-    time.sleep(3.0)
-
-    # FILL IN YOUR CODE HERE
-    # Please try both goTo and cmdPosition
-    goto_duration = math.sqrt(np.linalg.norm(data[0,:2]))/1.0
-    cf.goTo(goal = data[0,:], yaw = 0.0, duration = goto_duration, relative = False, groupMask = 0)
-    time.sleep(goto_duration)
-    # Move along a segment
+    #cf.setParam("ring/effect", 7)    
+    
+    # Takeoff
+    cf.takeoff(targetHeight = 1, duration = 5.0)
+    time.sleep(5.0)
+    
+    # GO to the first point of eight trajectory
+    cf.goTo(goal = eight_traj[0,:], yaw = 0.0, duration = 5.0, relative = False, groupMask = 0)
+    time.sleep(5.0)
+    
+    # Move along eight trajectory
     for i in range(200):
-        cf.cmdPosition(data[i,:], yaw = 0)
+        cf.cmdPosition(eight_traj[i,:], yaw = 0)
         time.sleep(0.1)
 
-    
+    # Go back to landing
+    cf.goTo(goal = [0.0, 1.5, 1.0], yaw = 0.0, duration = 5.0, relative = False, groupMask = 0)
+    time.sleep(5.0)
 
     cf.land(targetHeight = 0.0, duration = 5.0)
     time.sleep(5.0)
